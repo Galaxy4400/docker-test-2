b8611a6cfd38cbd9d09ff62ae81c179e18676a38
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-07-29 17:00:54 +0300

    api fix

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 
 app.get("/", (req, res) => {
 	res.json({ message: "Привет из backend снова!" });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 	const [string, setString] = useState("");
 
 	useEffect(() => {
-		fetch("http://localhost:3000")
+		fetch("/api")
 			.then((res) => res.json())
 			.then((data) => {
 				setString(data.message);
